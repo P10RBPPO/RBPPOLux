2da72cfbb8b791c8aabd28a10eed41494776c3dc
Taoshi <nicolaikh@live.dk>
2025-02-26 12:19:38 +0100

    Initial RobotController implementation

---
+++ Controllers/RobotController.py
@@ -0,0 +1,97 @@
+import heapq
+import math
+from lux.kit import obs_to_game_state, GameState
+from lux.config import EnvConfig
+from lux.utils import direction_to, my_turn_to_place_factory
+import numpy as np
+import sys
+import PathfindingResult
+
+class RobotController:
+    def __init__(self, game_state):
+        self.game_state = game_state
+        self.units = {}
+        self.unit_types = {}
+        self.unit_roles = {}
+
+    def add_unit(self, unit_id, unit, unit_type):
+        self.units[unit_id] = unit
+        self.unit_types[unit_id] = unit_type
+
+    def assign_role(self, unit_id, role):
+        self.unit_roles[unit_id] = role
+    
+    def update_game_state(self, new_game_state):
+        self.game_state = new_game_state
+
+    def control_units(self, actions):
+        factory_tiles = self.game_state.factory_tiles
+        factory_units = self.game_state.factory_units
+        ice_tile_locations = self.game_state.ice_tile_locations
+
+        for unit_id, unit in self.units.items():
+            role = self.unit_roles.get(unit_id, None)
+            if role == "Ice Miner":
+                self.control_ice_miner(unit_id, unit, factory_tiles, factory_units, ice_tile_locations, actions)
+        
+        # Resolve conflicts before returning actions
+        actions = self.resolve_conflicts(actions)
+        return actions
+
+    def control_ice_miner(self, unit_id, unit, factory_tiles, factory_units, ice_tile_locations, actions):
+        closest_factory = None
+        adjacent_to_factory = False
+        if len(factory_tiles) > 0:
+            factory_distances = np.mean((factory_tiles - unit.pos) ** 2, 1)
+            closest_factory_tile = factory_tiles[np.argmin(factory_distances)]
+            closest_factory = factory_units[np.argmin(factory_distances)]
+            adjacent_to_factory = np.mean((closest_factory_tile - unit.pos) ** 2) == 0
+
+            if unit.cargo.ice < 40:
+                ice_tile_distances = np.mean((ice_tile_locations - unit.pos) ** 2, 1)
+                closest_ice_tile = ice_tile_locations[np.argmin(ice_tile_distances)]
+                if np.all(closest_ice_tile == unit.pos):
+                    if unit.power >= unit.dig_cost(self.game_state) + unit.action_queue_cost(self.game_state):
+                        actions[unit_id] = [unit.dig(repeat=0, n=1)]
+                else:
+                    direction = direction_to(unit.pos, closest_ice_tile)
+                    move_cost = unit.move_cost(self.game_state, direction)
+                    if move_cost is not None and unit.power >= move_cost + unit.action_queue_cost(self.game_state):
+                        actions[unit_id] = [unit.move(direction, repeat=0, n=1)]
+            elif unit.cargo.ice >= 40:
+                direction = direction_to(unit.pos, closest_factory_tile)
+                if adjacent_to_factory:
+                    if unit.power >= unit.action_queue_cost(self.game_state):
+                        actions[unit_id] = [unit.transfer(direction, 0, unit.cargo.ice, repeat=0)]
+                else:
+                    move_cost = unit.move_cost(self.game_state, direction)
+                    if move_cost is not None and unit.power >= move_cost + unit.action_queue_cost(self.game_state):
+                        actions[unit_id] = [unit.move(direction, repeat=0, n=1)]
+    
+    def resolve_conflicts(self, actions):
+        target_positions = {}
+        resolved_actions = {}
+
+        for unit_id, action_list in actions.items():
+            if action_list:
+                action = action_list[0]
+                if action['type'] == 'move':
+                    target_pos = (self.units[unit_id].pos[0] + action['direction'][0], self.units[unit_id].pos[1] + action['direction'][1])
+                    if target_pos not in target_positions:
+                        target_positions[target_pos] = []
+                    target_positions[target_pos].append(unit_id)
+
+        for target_pos, unit_ids in target_positions.items():
+            if len(unit_ids) > 1:
+                unit_ids.sort(key=lambda uid: (self.unit_types[uid] != 'HEAVY', int(uid.split('_')[1])))
+                resolved_actions[unit_ids[0]] = actions[unit_ids[0]]
+                for uid in unit_ids[1:]:
+                    resolved_actions[uid] = []  # Cancel the move action by doing nothing
+            else:
+                resolved_actions[unit_ids[0]] = actions[unit_ids[0]]
+
+        for unit_id in actions:
+            if unit_id not in resolved_actions:
+                resolved_actions[unit_id] = actions[unit_id]
+
+        return resolved_actions
--- agent.py
+++ agent.py
@@ -6,6 +6,7 @@ from lux.utils import direction_to, my_turn_to_place_factory
 import numpy as np
 import sys
 import PathfindingResult
+import Controllers.RobotController as RobotController
 
 class Agent():
     def __init__(self, player: str, env_cfg: EnvConfig) -> None:
@@ -13,6 +14,7 @@ class Agent():
         self.opp_player = "player_1" if self.player == "player_0" else "player_0"
         np.random.seed(0)
         self.env_cfg: EnvConfig = env_cfg
+        self.robot_controller = RobotController.RobotController(None)  # Initialize with None game state
 
     def early_setup(self, step: int, obs, remainingOverageTime: int = 60):
         if step == 0:
@@ -39,15 +41,10 @@ class Agent():
 
     def act(self, step: int, obs, remainingOverageTime: int = 60):
         actions = dict()
-        
-        """
-        optionally do forward simulation to simulate positions of units, lichen, etc. in the future
-        from lux.forward_sim import forward_sim
-        forward_obs = forward_sim(obs, self.env_cfg, n=2)
-        forward_game_states = [obs_to_game_state(step + i, self.env_cfg, f_obs) for i, f_obs in enumerate(forward_obs)]
-        """
 
         game_state = obs_to_game_state(step, self.env_cfg, obs)
+        self.robot_controller.update_game_state(game_state)  # Update the RobotController with the new game_state
+
         factories = game_state.factories[self.player]
         game_state.teams[self.player].place_first
         factory_tiles, factory_units = [], []
@@ -64,39 +61,15 @@ class Agent():
         units = game_state.units[self.player]
         ice_map = game_state.board.ice
         ice_tile_locations = np.argwhere(ice_map == 1)
+
+        # Add units to the RobotController
         for unit_id, unit in units.items():
+            self.robot_controller.add_unit(unit_id, unit, unit.unit_type)
+            self.robot_controller.assign_role(unit_id, "Ice Miner")  # Assign role as Ice Miner for now
 
-            # track the closest factory
-            closest_factory = None
-            adjacent_to_factory = False
-            if len(factory_tiles) > 0:
-                factory_distances = np.mean((factory_tiles - unit.pos) ** 2, 1)
-                closest_factory_tile = factory_tiles[np.argmin(factory_distances)]
-                closest_factory = factory_units[np.argmin(factory_distances)]
-                adjacent_to_factory = np.mean((closest_factory_tile - unit.pos) ** 2) == 0
+        # Control units using the RobotController
+        actions = self.robot_controller.control_units(actions)
 
-                # previous ice mining code
-                if unit.cargo.ice < 40:
-                    ice_tile_distances = np.mean((ice_tile_locations - unit.pos) ** 2, 1)
-                    closest_ice_tile = ice_tile_locations[np.argmin(ice_tile_distances)]
-                    if np.all(closest_ice_tile == unit.pos):
-                        if unit.power >= unit.dig_cost(game_state) + unit.action_queue_cost(game_state):
-                            actions[unit_id] = [unit.dig(repeat=0, n=1)]
-                    else:
-                        direction = direction_to(unit.pos, closest_ice_tile)
-                        move_cost = unit.move_cost(game_state, direction)
-                        if move_cost is not None and unit.power >= move_cost + unit.action_queue_cost(game_state):
-                            actions[unit_id] = [unit.move(direction, repeat=0, n=1)]
-                # else if we have enough ice, we go back to the factory and dump it.
-                elif unit.cargo.ice >= 40:
-                    direction = direction_to(unit.pos, closest_factory_tile)
-                    if adjacent_to_factory:
-                        if unit.power >= unit.action_queue_cost(game_state):
-                            actions[unit_id] = [unit.transfer(direction, 0, unit.cargo.ice, repeat=0)]
-                    else:
-                        move_cost = unit.move_cost(game_state, direction)
-                        if move_cost is not None and unit.power >= move_cost + unit.action_queue_cost(game_state):
-                            actions[unit_id] = [unit.move(direction, repeat=0, n=1)]
         return actions
 
     def merge_action_queues(self, first, second):
