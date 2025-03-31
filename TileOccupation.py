0ad0c7c0bb3d06d16ae8a63a7ccd3efebe5afee0
Taoshix <nicolaikh@live.dk>
2025-03-31 15:39:24 +0200

    problem for tomorrow

--- TileOccupation.py
+++ TileOccupation.py
@@ -10,18 +10,4 @@ class TileOccupation:
         self.turns = turns  # List of turns
         self.owner = owner
     
-    def is_tile_occupied(self, tile, turn, unit_id=None):
-        """
-        Checks if a tile is occupied during a specific turn.
-        :param tile: Tuple (x, y) representing the tile's position.
-        :param turn: The turn to check.
-        :param unit_id: The unit ID of the robot trying to access the tile.
-        :return: True if the tile is occupied by another unit, False otherwise.
-        """
-        for occupation in self.tile_occupation:
-            if occupation.tile == tile and turn in occupation.turns:
-                # Allow access if the tile is occupied by the same unit
-                if unit_id and occupation.owner == unit_id:
-                    return False
-                return True
-        return False
+    
